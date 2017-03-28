0dd0187dd9d6ad9a853ff96b82cccf57e31a58a7
babak.bk.padashi@gmail.com <babak.bk.padashi@gmail.com>
2017-03-28 11:19:51 +0430

    first commit

--- app/src/main/java/bpadashi/ir/requester/MainActivity.java
+++ app/src/main/java/bpadashi/ir/requester/MainActivity.java
@@ -3,10 +3,11 @@ package bpadashi.ir.requester;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.ksoap2.serialization.SoapObject;
+
 import ir.bpadashi.requester.IRequestHandler;
 import ir.bpadashi.requester.Method;
 import ir.bpadashi.requester.Requester;
-import ir.bpadashi.requester.test.Company;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
         Requester aRequester = new Requester.RequesterBuilder(this)
 
                 .setUrl("http://onlinepakhsh.com/A_onlinepakhshService.asmx?WSDL")
-                .setMethodName("GetCompanyInfo")
+                .setMethodName("GetProducts")
                 .setNamespace("http://tempuri.org/")
-                .setSoapAction("http://tempuri.org/GetCompanyInfo")
+                .setSoapAction("http://tempuri.org/GetProducts")
 
                 .dontCache()
 
@@ -41,16 +42,17 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onResponse(Object context, StringBuilder response) {
+                    public void onResponse(Object context, Object response) {
 
-
-                        System.out.println(response);
 
                     }
 
                     @Override
                     public void onSuccess(Object context, Object model, boolean hasCache) {
-                        // TODO Auto-generated method stub
+
+                        SoapObject soapObject=(SoapObject)model;
+                        System.out.println(soapObject.getPropertyCount());
+
 
                     }
 
