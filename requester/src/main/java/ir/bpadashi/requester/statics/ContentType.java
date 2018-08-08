dbd1ca04234acefb5457b48d07edc3877290b35b
babakpadashi <babak.bk.padashi@gmail.com>
2018-08-08 21:29:29 +0430

    commit

--- requester/src/main/java/ir/bpadashi/requester/statics/ContentType.java
+++
@@ -1,7 +0,0 @@
-package ir.bpadashi.requester.statics;
-
-public enum ContentType {
-	
-	JSON,XML,TEXT,BYTE
-
-}
--- requester/src/main/java/ir/bpadashi/requester/statics/RequestMethod.java
+++
@@ -1,7 +0,0 @@
-package ir.bpadashi.requester.statics;
-
-public enum RequestMethod {
-	
-	GET,POST,SOAP
-
-}
